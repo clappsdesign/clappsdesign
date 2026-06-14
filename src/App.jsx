5a6544401f71863d69bff9259108ddabc55eb3f7
clappsdesign <clapps@clappsdesign.com>
2026-06-14 18:02:18 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+'Entrou aqui 2'
+
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
